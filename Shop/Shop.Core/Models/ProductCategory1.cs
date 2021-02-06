ed54d644fd2c82e12d29d343c932882e2c4ea404
IbtehazC <ibtehaz.chowdhury@gmail.com>
2021-02-06 16:38:16 +0600

    Product admin and category done

---
+++ Shop/Shop.Core/Models/ProductCategory1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Models
+{
+    public class ProductCategory
+    {
+        public String Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+        [Range(0, 1000)]
+        public Decimal Price  { get; set; }
+        public String Category { get; set; }
+        public String Image { get; set; }
+
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
